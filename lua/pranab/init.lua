f94981e64c59a317bc23c72afe012fdf844b701e
PranabZz <pranabkca321@gmail.com>
2026-01-26 19:36:36 +0545

    Fix: Migrate nvim-lspconfig usage to vim.lsp.config for Neovim 0.11+
    Updated LSP server configurations in lua/pranab/init.lua to use the
    new API 'vim.lsp.config()' and 'vim.lsp.enable()' as recommended
    for Neovim 0.11+ versions. This addresses the deprecation warning
    for 'require("lspconfig")'.
    
    Also removed 'dartls' from the mason-lspconfig ensure_installed list
    as it's a custom LSP configuration, not directly managed by mason-lspconfig.

--- lua/pranab/init.lua
+++ lua/pranab/init.lua
@@ -11,25 +11,49 @@ require("pranab.options")
 
 require("nvim-tree").setup()
 
--- 2. Define Server Configurations
--- Neovim 0.11+ can automatically find these in your 'lsp/' directory, 
--- but you can also enable them directly if using default settings:
-vim.lsp.enable({ 'lua_ls', 'dartls', 'gopls', 'pyright', 'clangd', 'intelephense', 'laravel_ls', 'tsserver' })
-
--- 3. Configure Native Autocompletion
--- This triggers the completion menu based on the LSP's 'triggerCharacters'
-vim.api.nvim_create_autocmd("LspAttach", {
-  desc = "LSP actions and completion",
-  callback = function(ev)
-    local client = vim.lsp.get_client_by_id(ev.data.client_id)
-    if client and client.supports_method("textDocument/completion") then
-      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = false })
-    end
-
-    -- Recommended: Set up default keymaps if they aren't already set
-    local opts = { buffer = ev.buf }
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-  end,
+-- 2. Configure Mason and LSP
+require("mason").setup()
+require("mason-lspconfig").setup({
+    ensure_installed = { "lua_ls", "gopls", "pyright", "clangd", "intelephense", "laravel_ls", "ts_ls" },
 })
 
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+local lspconfig = require("lspconfig")
+
+-- Configure LSPs
+vim.lsp.config('lua_ls', { capabilities = capabilities })
+vim.lsp.enable('lua_ls')
+vim.lsp.config('gopls', { capabilities = capabilities })
+vim.lsp.enable('gopls')
+vim.lsp.config('pyright', { capabilities = capabilities })
+vim.lsp.enable('pyright')
+vim.lsp.config('clangd', { capabilities = capabilities })
+vim.lsp.enable('clangd')
+vim.lsp.config('intelephense', { capabilities = capabilities })
+vim.lsp.enable('intelephense')
+vim.lsp.config('laravel_ls', { capabilities = capabilities })
+vim.lsp.enable('laravel_ls')
+vim.lsp.config('ts_ls', { capabilities = capabilities })
+vim.lsp.enable('ts_ls')
+
+-- Dart / Flutter
+vim.lsp.config('dartls', {
+    cmd = { "dart", "language-server", "--protocol=lsp" },
+    filetypes = { "dart" },
+    root_dir = lspconfig.util.root_pattern("pubspec.yaml"),
+    capabilities = capabilities,
+})
+vim.lsp.enable('dartls')
+
+-- 3. Configure LSP keymaps
+vim.api.nvim_create_autocmd("LspAttach", {
+    desc = "LSP actions",
+    callback = function(ev)
+        -- Recommended: Set up default keymaps if they aren't already set
+        local opts = { buffer = ev.buf }
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    end,
+})
