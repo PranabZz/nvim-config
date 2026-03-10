40b4f601b6b8e0af1b0199d5d47cf2e2e955dfca
PranabZz <pranabkca321@gmail.com>
2026-03-10 16:52:27 +0545

    00101001 00001011 11100110

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -1,8 +1,8 @@
 function ColorMyPixels(color)
-	color = color or "catppuccin-mocha"
-	vim.cmd.colorscheme(color)
-	vim.api.nvim_set_hl(0, "Normal", { bg ="none"})
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg ="none"})
-end 
+    color = color or "kanagawa"
+    vim.cmd.colorscheme(color)
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
 
 ColorMyPixels()
--- lua/pranab/init.lua
+++ lua/pranab/init.lua
@@ -1,43 +1,37 @@
--- ~/.config/nvim/init.lua
--- 0. Completely disable netrw and its plugin at the very top of init.lua
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+require('pranab.remap')
+require('pranab.packer')
+require('pranab.options')
+require('pranab.file_management')
 
--- 1. Load your other modules
-require("pranab.remap")
-require("pranab.packer")
-require("pranab.options")
-require("pranab.file_management")
+require('nvim-tree').setup()
 
-require("nvim-tree").setup()
-
--- 2. Configure Mason and LSP
-require("mason").setup({
-    ensure_installed = { "dart-debug-adapter", "dcm" },
+require('mason').setup({
+    ensure_installed = { 'dart-debug-adapter', 'dcm' },
 })
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-require("mason-lspconfig").setup({
-    ensure_installed = { "lua_ls", "gopls", "pyright", "clangd", "intelephense", "laravel_ls", "ts_ls", "dartls" },
+require('mason-lspconfig').setup({
+    ensure_installed = { 'lua_ls', 'gopls', 'pyright', 'clangd', 'intelephense', 'laravel_ls', 'ts_ls', 'dartls', 'kotlin_language_server' },
     handlers = {
         function(server_name)
-            require("lspconfig")[server_name].setup({
-                capabilities = capabilities,
-            })
+            vim.lsp.config(server_name, { capabilities = capabilities })
+            vim.lsp.enable(server_name)
         end,
     },
 })
 
-
--- 3. Configure LSP keymaps
-vim.api.nvim_create_autocmd("LspAttach", {
-    desc = "LSP actions",
+vim.api.nvim_create_autocmd('LspAttach', {
+    desc = 'LSP actions',
     callback = function(ev)
-        -- Recommended: Set up default keymaps if they aren't already set
         local opts = { buffer = ev.buf }
         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
         vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
     end,
 })
+
+vim.lsp.config('sourcekit', { capabilities = capabilities })
+vim.lsp.enable('sourcekit')
